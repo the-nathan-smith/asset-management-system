839f3270daaa0a44545a8b0ab11edf22b58fa83f
Nathan Smith <nathan.smith12@nhs.net>
2022-03-23 20:15:35 +0000

    Create laptop and mobile models

---
+++ inventory/models.py
@@ -0,0 +1,46 @@
+from sys import flags
+from telnetlib import STATUS
+from django.db import models
+
+class Laptop(models.Model):
+    type_choices = (
+        ('SURFACEPRO', 'Surface Pro'),
+        ('MACBOOK', 'MacBook'),
+        ('THINKPAD', 'Thinkpad')
+    )
+    type = models.CharField(max_length=100, choices=type_choices, blank=False)
+
+    owner = models.CharField(max_length=100, blank=False) #This needs changing to a User
+
+    status_choices = (
+        ('AVAILABLE', 'Device is available to be assigned'),
+        ('ASSIGNED', 'Device is assigned to a staff member'),
+        ('SETUP_REQUIRED', 'This device needs setting up before it can be assigned'),
+    )
+    status =models.CharField(max_length=100, choices=status_choices, blank=False)
+
+
+    def __str__(self):
+        return 'Type : {0} Owner: {1} Status: {2}'.format(self.type, self.owner, self.status)
+
+
+class Mobile(models.Model):
+    type_choices = (
+        ('ANDROID', 'Android'),
+        ('IPHONE', 'iPhone'),
+        ('SAMSUNG', 'Samsung')
+    )
+    type = models.CharField(max_length=100, choices=type_choices, blank=False)
+
+    owner = models.CharField(max_length=100, blank=False) #This needs changing to a User
+
+    status_choices = (
+        ('AVAILABLE', 'Device is available to be assigned'),
+        ('ASSIGNED', 'Device is assigned to a staff member'),
+        ('SETUP_REQUIRED', 'This device needs setting up before it can be assigned'),
+    )
+    status =models.CharField(max_length=100, choices=status_choices, blank=False)
+
+
+    def __str__(self):
+        return 'Type : {0} Owner: {1} Status: {2}'.format(self.type, self.owner, self.status)
