0445724c684f4358fb5fa1b1735633a426a2d308
Nathan Smith <nathan.smith12@nhs.net>
2022-03-23 22:10:49 +0000

    Create add laptop and mobile buttons

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,8 @@ from .views import *
 urlpatterns = [
     re_path(r'^$', index, name='index'),
     re_path(r'^display_laptops$', display_laptops, name='display_laptops'),
-    re_path(r'^display_mobiles$', display_mobiles, name='display_mobiles')
+    re_path(r'^display_mobiles$', display_mobiles, name='display_mobiles'),
+
+    re_path(r'^add_laptop$', add_laptop, name='add_laptop'),
+    re_path(r'^add_mobile$', add_mobile, name='add_mobile'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from inventory.forms import *
 from .models import *
 
 def index(request):
@@ -9,6 +11,7 @@ def display_laptops(request):
     items = Laptop.objects.all()
     context = {
         'items' : items,
+        'header' : 'Laptops',
     }
     return render(request, 'index.html', context)
 
@@ -17,5 +20,27 @@ def display_mobiles(request):
     items = Mobile.objects.all()
     context = {
         'items' : items,
+        'header' : 'Mobiles',
     }
     return render(request, 'index.html', context)
+
+
+def add_device(request, cls):
+    if request.method == "POST":
+        form = cls(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    else :
+        form = cls()
+        return render(request, 'add_new.html', {'form': form})
+
+
+def add_laptop(request):
+    return add_device(request, LaptopForm)
+
+
+def add_mobile(request):
+    return add_device(request, MobileForm)
