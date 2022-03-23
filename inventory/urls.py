d4b9ec108b5ce413d241e61c0d9be0a509a1e490
Nathan Smith <nathan.smith12@nhs.net>
2022-03-23 21:06:11 +0000

    Show devices list on home page

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,8 @@
 from django.urls import re_path
-from .views import index
+from .views import *
 
 urlpatterns = [
-    re_path(r'^$', index, name='index')
+    re_path(r'^$', index, name='index'),
+    re_path(r'^display_laptops$', display_laptops, name='display_laptops'),
+    re_path(r'^display_mobiles$', display_mobiles, name='display_mobiles')
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
     return render(request, 'index.html')
+
+
+def display_laptops(request):
+    items = Laptop.objects.all()
+    context = {
+        'items' : items,
+    }
+    return render(request, 'index.html', context)
+
+
+def display_mobiles(request):
+    items = Mobile.objects.all()
+    context = {
+        'items' : items,
+    }
+    return render(request, 'index.html', context)
