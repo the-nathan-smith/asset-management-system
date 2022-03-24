71479189dde87cf7bee18b39a84df41beb8dbc85
Nathan Smith <nathan.smith12@nhs.net>
2022-03-24 10:45:36 +0000

    Add import export func in admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import *
 
 @admin.register(Laptop, Mobile)
-class ViewAdmin(admin.ModelAdmin):
+class ViewAdmin(ImportExportModelAdmin):
     pass
