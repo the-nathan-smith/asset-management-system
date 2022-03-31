22b93af91f6bf17b4cc8c6741a6a503a334029b3
Nathan Smith <nathan.smith12@nhs.net>
2022-03-31 23:50:01 +0100

    Edit redirects for delete and edit views

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
 
 from inventory.forms import *
 from .models import *
@@ -49,34 +50,44 @@ def add_mobile(request):
     return add_device(request, MobileForm)
 
 
-def edit_device(request, pk, model, cls):
+def edit_device(request, pk, model, form, deviceType):
     item = get_object_or_404(model, pk=pk)
+    items = model.objects.all()
+    context = {
+        'items': items,
+        'header': deviceType
+    }
 
     if request.method == 'POST':
-        form = cls(request.POST, instance=item)
+        form = form(request.POST, instance=item)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            messages.success(request, ("Device edited successfully."))
+            return render(request, 'inventory/index.html', context)
 
     else:
-        form = cls(instance=item)
+        form = form(instance=item)
         return render(request, 'inventory/edit_item.html', {'form': form})
 
 
 def edit_laptop(request, pk):
-    return edit_device(request, pk, Laptop, LaptopForm)
+    deviceType = "Laptops"
+    return edit_device(request, pk, Laptop, LaptopForm, deviceType)
 
 
 def edit_mobile(request, pk):
-    return edit_device(request, pk, Mobile, MobileForm)
+    deviceType = "Mobiles"
+    return edit_device(request, pk, Mobile, MobileForm, deviceType)
 
 
 def delete_laptop(request, pk):
     Laptop.objects.filter(id=pk).delete()
     items = Laptop.objects.all()
     context = {
-        'items': items
+        'items': items,
+        'header': 'Laptops'
     }
+    messages.success(request, ("Device successfully deleted."))
 
     return render(request, 'inventory/index.html', context)
 
@@ -85,7 +96,9 @@ def delete_mobile(request, pk):
     Mobile.objects.filter(id=pk).delete()
     items = Mobile.objects.all()
     context = {
-        'items': items
+        'items': items,
+        'header': 'Mobiles'
     }
+    messages.success(request, ("Device successfully deleted."))
 
     return render(request, 'inventory/index.html', context)
